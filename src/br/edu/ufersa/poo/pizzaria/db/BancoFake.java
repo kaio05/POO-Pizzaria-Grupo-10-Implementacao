55044f5b17b2512255434985fec8e43f5ca5274d
Kaio Silva <kaiofernando05cont@gmail.com>
2025-05-20 17:56:04 -0300

    feat: implementa classe Usuario

--- src/br/edu/ufersa/poo/pizzaria/db/BancoFake.java
+++ src/br/edu/ufersa/poo/pizzaria/db/BancoFake.java
@@ -1,8 +1,47 @@
 package br.edu.ufersa.poo.pizzaria.db;
 import java.util.List;
+import java.util.UUID;
 import java.util.ArrayList;
 import br.edu.ufersa.poo.pizzaria.entity.*;;
 
 public class BancoFake {
     public static List<Cliente> clientes = new ArrayList<Cliente>();
+
+    public static List<Usuario> usuarios = new ArrayList<Usuario>();
+
+    public static boolean exists(UUID id) {
+        return true;
+    }
+
+    public static boolean exists(String nome) {
+        return true;
+    }
+
+    public static boolean exists(String nome, String endereco, String cpf, String telefone) {
+        return true;
+    }
+
+    public static boolean authenticate(String nome, String senha) {
+        return true;
+    }
+
+    public static void save(Cliente cliente) {
+        System.out.println("Salvo com sucesso!");
+    }
+
+    public static void save(Usuario usuario) {
+        System.out.println("Salvo com sucesso!");
+    }
+
+    public static void update(Cliente cliente) {
+        System.out.println("Salvo com sucesso!");
+    }
+
+    public static void update(Usuario usuario) {
+        System.out.println("Salvo com sucesso!");
+    }
+
+    public static void remove(UUID id) {
+        System.out.println("Removido com sucesso!");
+    }
 }
---
+++ src/br/edu/ufersa/poo/pizzaria/entity/Usuario.java
@@ -0,0 +1,92 @@
+package br.edu.ufersa.poo.pizzaria.entity;
+
+import java.util.UUID;
+
+import br.edu.ufersa.poo.pizzaria.db.BancoFake;
+
+public class Usuario {
+    private UUID id;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Usuario(String nome, String email, String senha) {
+        this.id = UUID.randomUUID();
+        setNome(nome);
+        setEmail(email);
+        setSenha(senha);
+    }
+
+    public Usuario(UUID id, String nome, String email, String senha) {
+        setId(id);
+        setEmail(email);
+        setNome(nome);
+        setSenha(senha);
+    }
+
+    public void cadastrar(String nome, String email, String senha) {
+        Usuario usuario = new Usuario(nome, email, senha);
+        if (BancoFake.exists(nome)) {
+            throw new Error("Usuário já cadastrado! Tente fazer login");
+        }
+        BancoFake.save(usuario);
+    }
+    
+    public void login(String nome, String senha) {
+        if (!BancoFake.exists(nome)) {
+            throw new Error("Usuário não cadastrado! Cadastre-se!");
+        }
+        if (!BancoFake.authenticate(nome, senha)) {
+            throw new Error("Senha incorreta!");
+        }
+        System.out.println("Autenticado com sucesso!");
+    }
+    
+    public void editar(UUID id, String nome, String email, String senha) {
+        Usuario usuario = new Usuario(id, nome, email, senha);
+        if (!BancoFake.exists(id)) {
+            throw new Error("Usuário não cadastrado! Cadastre-se!");
+        }
+        BancoFake.update(usuario);
+    }
+
+    public void excuir(UUID id) {
+        if (!BancoFake.exists(id)) {
+            throw new Error("Usuário não cadastrado! Cadastre-se!");
+        }
+        BancoFake.remove(id);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    
+}
